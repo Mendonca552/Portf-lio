616d49557676f092b3f2eb97863dcdecf7c819f5
Maria Eduarda Pereira Mendonça <mariaeduarda382749@gmail.com>
2026-05-04 20:43:27 +0000

    Potifolio

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -21,9 +21,19 @@ botao.addEventListener("click", () => {
 const NOME = "Maria Dudinha";
 let tituloProfissional = "Garota de programa";
 let minhaBio = "Procuro um rumo nesse grande vale da estranheza" + 
-        "Tenho um thor";
-let anoFormatura = 2026; 
+        " Tenho um thor";
+let anoFormatura = 2026;
+let mesFormatura = 12; 
+let diaFormatura = 31; 
 let anoIngresso = 2025;
+let mesIngresso = 1;
+let diaIngresso = 1;
+
+let hoje = new Date();
+let mesAtual = hoje.getMonth() +1;// começa no mês 0
+let anoAual = hoje.getFullYear(); // ano atual 
+let diaAtual = hoje.getDate(); // dia atual 
+
 let indefinido;
 let nulo = null; 
 let curso = {
@@ -44,3 +54,6 @@ document.getElementById("meuNome").innerText = NOME;
 document.getElementById("tituloProfissional").innerText = tituloProfissional;
 document.getElementById("minhaBio").innerText = minhaBio; 
 document.getElementById("anoFormatura").innerText = "Ano de formatura: " + anoFormatura;
+document.getElementById("anoIngresso").innerText = "Ano de ingresso: " + anoIngresso;
+document.getElementById("curso").innerText = curso;
+document.getElementById("tempoRestante").innerText = `Tempo restante para formatura: ${anoFormatura - anoIngresso}`
